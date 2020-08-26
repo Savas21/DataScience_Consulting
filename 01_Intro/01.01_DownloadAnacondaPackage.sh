2fcb225ef731745c4119eda7794627f247aa045a
Savas Karakus <savas.kara21@gmail.com>
2020-08-25 19:09:20 -0700

    Initial meeting set up notes

---
+++ 01_Intro/01.01_DownloadAnacondaPackage.sh
@@ -0,0 +1,5 @@
+## Follow-up the instructions provided in the below link to download Anaconda Package
+https://docs.anaconda.com/anaconda/install/mac-os/
+
+## Youtube link for your easy reference
+https://www.youtube.com/watch?v=daVgEXjv6DE
---
+++ 01_Intro/01.02_Github_Assignment.sh
@@ -0,0 +1,9 @@
+# Set up Github account
+- Sign up for github account (https://github.com/)
+- Create your first project
+- Clone the project in your local 
+- Make changes in the file 
+- Push local changes to the github 
+
+## Youtube link for your easy reference
+https://www.youtube.com/watch?v=iv8rSLsi1xo
